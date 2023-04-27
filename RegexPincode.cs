6a6d4bb897584596d9eb1f8387232af0eacb54ec
Mahesh <Maheshk2104@gmail.com>
2023-04-27 16:27:51 +0530

    UC4

---
+++ RegexPincode.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegexExamples
+{
+    internal class RegexPincode
+    {
+        public static void Main(string[] args)
+        {
+
+            string pinPattern = @"^(\d{6}|\d{3}\s*\d{3})$";
+            string pinCode1 = "400088";
+            string pinCode2 = "400 088";
+
+            if (Regex.IsMatch(pinCode1, pinPattern))
+            {
+                Console.WriteLine("Valid PIN code: " + pinCode1);
+            }
+            else
+            {
+                Console.WriteLine("Invalid PIN code: " + pinCode1);
+            }
+
+            if (Regex.IsMatch(pinCode2, pinPattern))
+            {
+                Console.WriteLine("Valid PIN code: " + pinCode2);
+            }
+            else
+            {
+                Console.WriteLine("Invalid PIN code: " + pinCode2);
+            }
+        }
+    }
+}
