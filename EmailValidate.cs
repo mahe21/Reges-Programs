aa25dbe045f529262900062001abae0ff7667426
Mahesh <Maheshk2104@gmail.com>
2023-04-27 16:55:23 +0530

    EmailValidate

---
+++ EmailValidate.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegexExamples
+{
+    internal class EmailValidate
+    {
+        public static void Main(string[] args)
+        {
+            string EmailPattern = @"^[a-z]{3}.[a-z]{3}@[a-z]{3,11}\.co$";
+            Console.WriteLine("Enter Email Addres: ");
+            string emailAddress = Console.ReadLine();
+
+            if (Regex.IsMatch(emailAddress, EmailPattern))
+            {
+                Console.WriteLine("Valid email address.");
+            }
+            else
+            {
+                Console.WriteLine("Invalid email address.");
+            }
+        }
+    }
+}
