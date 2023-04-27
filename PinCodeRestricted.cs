f527c5cb5b3f973279016a12cfebef3e91b0234b
Mahesh <Maheshk2104@gmail.com>
2023-04-27 16:12:11 +0530

    Uc2

---
+++ PinCodeRestricted.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegexExamples
+{
+    internal class PinCodeRestricted
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Pincode: ");
+            string pinCode = Console.ReadLine();
+            string PincodeRegex = "^[0-9]{6}$";
+
+            if(Regex.IsMatch(pinCode, PincodeRegex))
+            {
+                Console.WriteLine("Valid pincode.");
+            }
+            else
+            {
+                Console.WriteLine("Invalid Pincode.");
+            }
+        }
+    }
+}
