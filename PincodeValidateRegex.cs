b0a22bc0cdc4a2e46865b3d1a5870fd3bc7becfa
Mahesh <Maheshk2104@gmail.com>
2023-04-27 16:09:02 +0530

    Uc1

---
+++ PincodeValidateRegex.cs
@@ -0,0 +1,24 @@
+﻿/*using System.Security.Cryptography.X509Certificates;
+using System.Text.RegularExpressions;
+
+namespace RegexExamples
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter PinCode:");
+            string pincode=Console.ReadLine();
+            Regex Pincoderegex = new Regex(Patterns.PinCodePattern);
+            var Pincodeflag= Pincoderegex.IsMatch(pincode);
+            Console.WriteLine(Pincodeflag);
+
+        }
+
+        public class Patterns
+        {
+            public static string PinCodePattern = "^[0-9]{6}$";
+        }
+    }
+}*/
+            
